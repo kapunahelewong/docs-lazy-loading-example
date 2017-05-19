3cf78531b28b9ef07e8248543b8ed2e55914d831
Kapunahele Wong <kapunahelewong@gmail.com>
2017-05-19 16:02:57 -0400

    add lazy loaded and eagerly loaded modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { InventoryModule } from './inventory/inventory.module';
+import { InventoryListComponent } from './inventory/inventory-list/inventory-list.component';
+
+
 const routes: Routes = [
   {
+    path: 'customers',
+    loadChildren: './customers/customers.module#CustomersModule'
+  },
+  {
+    path: 'orders',
+    loadChildren: './orders/orders.module#OrdersModule'
+  },
+  {
+    path: 'inventory',
+    component: InventoryListComponent
+  },
+  {
     path: '',
-    children: []
+    redirectTo: '',
+    pathMatch: 'full'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    InventoryModule
+  ],
   exports: [RouterModule],
   providers: []
 })
